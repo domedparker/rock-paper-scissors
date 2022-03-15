3768ef9da3a545ee7727b96af9d45a01958219b7
Dominic Parker <dominic.e.parker@gmail.com>
2022-03-15 13:05:09 +0000

    updates to main.js to make code work rudimentally

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 const choice = ['rock', 'paper', 'scissors'];
+const computerSelection = computerChoice();
+let playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
+    while((playerSelection === '') || (playerSelection === null)){
+        playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
+    }
 
-function playerChoice(){}
+function playRound(playerSelection, computerSelection){
+  if (playerSelection === computerSelection) {
+      alert('It\'s a Tie!')
+  } else if (
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        alert(`You win! ${playerSelection} beats ${computerSelection}!`);
+  } else if (
+      (playerSelection === 'paper' && computerSelection === 'scissors') ||
+      (playerSelection === 'rock' && computerSelection === 'paper') ||
+      (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        alert(`You lose! ${computerSelection} beats ${playerSelection}!`);
+  }
+ }
 
 function computerChoice(){
-return choice[Math.floor(Math.random() * choice.length)]
+    return choice[Math.floor(Math.random() * choice.length)]
 }
+
+playRound(playerSelection, computerSelection);
